0af7e3b078ffbde0bad2f737ddc625eae14cd90e
ccx1965 <claudio.ccx@gmail.com>
2024-12-26 00:03:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Escucha el evento click en el botón
+document
+  .getElementById("redirectButton")
+  .addEventListener("click", function () {
+    // Redirige a la página de pedidos
+    window.location.href = "https://ccx1965.github.io/pedidos/";
+  });
